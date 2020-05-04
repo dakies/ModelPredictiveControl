566ebb7ed5eabd5cb543b4110045cfe6f3ff0b9e
Daniel <daniekie@studetn.ethz.ch>
2020-05-04 20:39:37 +0200

    -Removed harcoding -Fixed Gu

--- project_template/run_simulations.m
+++ project_template/run_simulations.m
@@ -13,7 +13,7 @@ T0_1 = dev + [-21; 0.3; 7.32];
 
 %T5
 % % execute simulation starting from T0_1 using lqr controller with scenario 1
-% [T, p] = simulate_truck(T0_1, @controller_lqr, scen1);
+[T, p] = simulate_truck(T0_1, @controller_lqr, scen1);
 % % Controller works with constraints. Norm constraint from question checked
 % % t_30 = [-20.64; 0.5786; 7.475]
 % % norm(T_sp-t_30)<0.2*norm([3;1;0])
@@ -22,4 +22,4 @@ T0_1 = dev + [-21; 0.3; 7.32];
 % [T, p] = simulate_truck(T0_1, @controller_mpc_1, scen1);
 
 %T11
-[T, p] = simulate_truck(T0_1, @controller_mpc_2, scen1);
+% [T, p] = simulate_truck(T0_1, @controller_mpc_2, scen1);
